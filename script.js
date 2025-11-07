b3981edad8cf135eb0316e763982bdeae1a3d767
hassanruqa <182183409+hassanruqa@users.noreply.github.com>
2025-11-07 09:37:37 -0500

    Refactor D3.js chart code for better performance
    Updated the D3.js chart implementation to use the latest API methods and improved data handling.

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-
 <script>
-
-
-
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-
+// Create the SVG container
 var svg = d3.select("#chart")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
+// Load the CSV data
 d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
 
+  // Convert Value to number
+  data.forEach(function(d) {
+    d.Value = +d.Value;
+  });
 
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(data.map(function(d) { return d.Country; }))
-  .padding(0.2);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+  // ----- X Axis -----
+  var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], 0.2)
+    .domain(data.map(function(d) { return d.Country; }));
 
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.svg.axis().scale(x).orient("bottom"))
+    .selectAll("text")
+      .attr("transform", "rotate(-45)")
+      .style("text-anchor", "end");
 
-var y = d3.scaleLinear()
-  .domain([0, 13000])
-  .range([ height, 0]);
-svg.append("g")
-  .call(d3.axisLeft(y));
+  // ----- Y Axis -----
+  var y = d3.scale.linear()
+    .domain([0, d3.max(data, function(d) { return d.Value; })])
+    .range([height, 0]);
 
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(d3.svg.axis().scale(y).orient("left"));
 
-svg.selectAll("mybar")
-  .data(data)
-  .enter()
-  .append("rect")
-    .attr("x", function(d) { return x(d.Country); })
-    .attr("y", function(d) { return y(d.Value); })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(d.Value); })
-    .attr("fill", "#69b3a2")
-
-})
-
+  // ----- Bars -----
+  svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.Country); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.Value); })
+      .attr("height", function(d) { return height - y(d.Value); })
+      .style("fill", "#69b3a2");
+});
 </script>
-
