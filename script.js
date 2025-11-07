918e7bef81e7da83cd430e453b00968e7891c0df
hassanruqa <182183409+hassanruqa@users.noreply.github.com>
2025-11-07 14:13:20 -0500

    Insert D3.js script for loading CSV data
    Added D3.js script to load and filter Disney TV show data.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 <script>
 
+    d3.csv("data/disney.csv", function(data)
+    {
+        var filtered_data = data.filter( d => d.type === "Tv Show" && d.rating.startswith("TV"))
+    
+    
+    
+    
+    
+    
+    }
 
 
     
     </script>
+
