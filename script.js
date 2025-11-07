48b5fb6e980ca6998a0ec74d7fbea33be9864306
hassanruqa <182183409+hassanruqa@users.noreply.github.com>
2025-11-07 09:30:46 -0500

    Remove HTML tags from script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 <script>
 
 
-// set the dimensions and margins of the graph
+
 var margin = {top: 30, right: 30, bottom: 70, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
+
 var svg = d3.select("#chart")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -16,10 +16,10 @@ var svg = d3.select("#chart")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-// Parse the Data
+
 d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
 
-// X axis
+
 var x = d3.scaleBand()
   .range([ 0, width ])
   .domain(data.map(function(d) { return d.Country; }))
@@ -31,14 +31,14 @@ svg.append("g")
     .attr("transform", "translate(-10,0)rotate(-45)")
     .style("text-anchor", "end");
 
-// Add Y axis
+
 var y = d3.scaleLinear()
   .domain([0, 13000])
   .range([ height, 0]);
 svg.append("g")
   .call(d3.axisLeft(y));
 
-// Bars
+
 svg.selectAll("mybar")
   .data(data)
   .enter()
@@ -52,3 +52,4 @@ svg.selectAll("mybar")
 })
 
 </script>
+
