97d09d29f6699e9c92d7de1976d4fcb0a3e82a57
hassanruqa <182183409+hassanruqa@users.noreply.github.com>
2025-11-08 10:56:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,19 @@
 
     d3.csv("data/disney.csv", function(data)
     {
-        var filtered_data = data.filter( d => d.type === "Tv Show" && d.rating.startswith("TV"))
+        var filtered_data = data.filter(function(d) { 
+            return d.type === "Tv Show" && d.rating.startswith("TV");
+        });
+    
+    console.log(filtered_data);
     
     
     
     
-    
-    
-    }
+    });
 
 
     
     </script>
+
 
