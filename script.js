e12ba10378b1286e7a1414e3b939b33f300bce90
hassanruqa <182183409+hassanruqa@users.noreply.github.com>
2025-11-24 12:45:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -65,8 +65,8 @@ var hideTooltip = function(d) {
     var initialData = allYearsData[0].counts;
 
     var margin = {top: 40, right: 20, bottom: 50, left: 50};
-    var width = 900;
-    var height = 800;
+    var width = 700 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
 
     // Scales
     var xScale = d3.scale.ordinal()
@@ -79,8 +79,6 @@ var hideTooltip = function(d) {
 
     // SVG
     var svg = d3.select("#main")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -136,7 +134,7 @@ var hideTooltip = function(d) {
         .attr("class", "x label")
         .attr("text-anchor", "middle")
         .attr("x", width/2)
-        .attr("y", height + 40)
+        .attr("y", height + 50)
         .text("TV Rating Category");
 
     svg.append("text")
@@ -144,12 +142,13 @@ var hideTooltip = function(d) {
         .attr("text-anchor", "middle")
         .attr("transform", "rotate(-90)")
         .attr("x", -height/2)
-        .attr("y", -40)
+        .attr("y", -45)
         .text("Number of Series");
     
     drawBarChart(allYearsData[0].counts);
 
 }); // end of dc.csv
+
 
 
 
